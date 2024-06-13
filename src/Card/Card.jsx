30302695a0b530f289805e58980bd80a5423458a
David Gaona <davidgaonahenao@gmail.com>
2024-06-13 08:18:02 -0500

    Function to the Card

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
-import './Card.css'
-import imgCard from '../assets/images/Photo.png'
+import React from "react";
+import "./Card.css";
+import imgCard from "../assets/images/Photo.png";
+import { useState } from "react";
 
 export const Card = () => {
+  const [Color, setColor] = useState("");
+  onclick[input] = (Color) => {
+    HashChangeEvent, setColor, Color;
+  };
+
   return (
     <>
       <label htmlFor="">Elige el color de la card</label>
       <input type="color" />
-      <div className='card'>
+      <div className="card">
         <div className="header-card"></div>
         <div className="main-card">
-          <img src={imgCard} alt="Photo" /> 
+          <img src={imgCard} alt="Photo" />
         </div>
         <div className="footer-card"></div>
-      </div>    
+      </div>
     </>
-  )
-}
+  );
+};
